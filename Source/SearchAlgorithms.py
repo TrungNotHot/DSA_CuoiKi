e030c093ce9bcb53bf959014fc836168e1a17876
TrungNotHot <trung2003lucky@gmail.com>
2022-12-09 23:35:51 +0700

    Chinh sua lai ham cu

--- Source/SearchAlgorithms.py
+++ Source/SearchAlgorithms.py
@@ -1,6 +1,8 @@
 from Space import *
 from Constants import *
 from pygame.locals import *
+
+
 def wait():
     while True:
         for event in pygame.event.get():
@@ -9,93 +11,112 @@ def wait():
                 exit()
             if event.type == KEYDOWN and event.key == K_f:
                 return
-def DFS(g:Graph, sc:pygame.Surface):
+
+
+def DFS(g: Graph, sc: pygame.Surface):
     print('Implement DFS algorithm')
 
-    open_set = [g.start.value] #stack
+    open_set = [g.start.value]  # stack
     closed_set = []
     father = [-1]*g.get_len()
 
-    for indx in range(0,len(father)):
-        father[indx]=open_set.pop(-1)
-        while father[indx] in closed_set:
-            father[indx]=open_set.pop(-1)
+    for indx in range(0, len(father)):
+        if (len(open_set) == 0):
+            break
+        if (open_set[-1] in father):
+            open_set.pop(-1)
+            continue
 
-        if g.grid_cells[father[indx]].color not in [orange,purple]:
+        father[indx] = open_set.pop(-1)
+        if g.grid_cells[father[indx]].color not in [orange, purple]:
             g.grid_cells[father[indx]].set_color(yellow)
             g.draw(sc)
 
         if g.is_goal(g.grid_cells[father[indx]]):
             for i in range(indx):
-                pygame.draw.line(sc,green,(g.grid_cells[father[i]].x,g.grid_cells[father[i]].y),(g.grid_cells[father[i+1]].x,g.grid_cells[father[i+1]].y))
-                if g.grid_cells[father[i]].color not in [orange,purple]:
+                pygame.draw.line(sc, green, (g.grid_cells[father[i]].x, g.grid_cells[father[i]].y), (
+                    g.grid_cells[father[i+1]].x, g.grid_cells[father[i+1]].y))
+                if g.grid_cells[father[i]].color not in [orange, purple]:
                     g.grid_cells[father[i]].set_color(grey)
                 g.draw(sc)
             wait()
 
         for neighbor in g.get_neighbors(g.grid_cells[father[indx]]):
             if neighbor.value not in closed_set:
-                open_set.append(neighbor.value) #Thứ tự ưu tiên tăng dần từ up, down, left, right, up_left, up_right, down_left, down_right
-                if neighbor.color==green:
+                # Thứ tự ưu tiên tăng dần từ up, down, left, right, up_left, up_right, down_left, down_right
+                open_set.append(neighbor.value)
+                if neighbor.color == green:
                     neighbor.set_color(red)
                     g.draw(sc)
 
         closed_set.append(father[indx])
 
-        if g.grid_cells[father[indx]].color not in [orange,purple]:
+        if g.grid_cells[father[indx]].color not in [orange, purple]:
             g.grid_cells[father[indx]].set_color(blue)
             g.draw(sc)
 
     raise NotImplementedError('Not implemented')
 
-def BFS(g:Graph, sc:pygame.Surface):
+
+def findFather(g: Graph, cur: Node, father, sc: pygame.Surface):
+    if cur == g.start:
+        wait()
+    pygame.draw.line(sc, green, (cur.x, cur.y), (
+        g.grid_cells[father[cur.value]].x, g.grid_cells[father[cur.value]].y))
+    if cur.color not in [orange, purple]:
+        cur.set_color(grey)
+    g.draw(sc)
+    return findFather(g, g.grid_cells[father[cur.value]], father, sc)
+
+
+def BFS(g: Graph, sc: pygame.Surface):
     print('Implement BFS algorithm')
 
-    open_set = [g.start.value] #queue
+    open_set = [g.start.value]  # queue
     closed_set = []
     father = [-1]*g.get_len()
 
-    for indx in range(0,len(father)):
-        father[indx]=open_set.pop(0)
+    for indx in range(0, len(father)):
+        if (len(open_set) == 0):
+            break
+        current = open_set.pop(0)
 
-        if g.grid_cells[father[indx]].color not in [orange,purple]:
-            g.grid_cells[father[indx]].set_color(yellow)
+        if g.grid_cells[current].color not in [orange, purple]:
+            g.grid_cells[current].set_color(yellow)
             g.draw(sc)
 
-        if g.is_goal(g.grid_cells[father[indx]]):
-            for i in range(indx):
-                pygame.draw.line(sc,green,(g.grid_cells[father[i]].x,g.grid_cells[father[i]].y),(g.grid_cells[father[i+1]].x,g.grid_cells[father[i+1]].y))
-                if g.grid_cells[father[i]].color not in [orange,purple]:
-                    g.grid_cells[father[i]].set_color(grey)
-                g.draw(sc)
-            wait()
+        if g.is_goal(g.grid_cells[current]):
+            findFather(g, g.grid_cells[current], father, sc)
 
-        for neighbor in g.get_neighbors(g.grid_cells[father[indx]]):
+        for neighbor in g.get_neighbors(g.grid_cells[current]):
             if neighbor.value not in closed_set:
-                if neighbor.color==green:
-                    open_set.append(neighbor.value) #Thứ tự thêm là: up, down, left, right, up_left, up_right, down_left, down_right
-                    neighbor.set_color(red)
-                    g.draw(sc)
-                if neighbor.color==purple:
+                if neighbor.color == green:
+                    # Thứ tự thêm là: up, down, left, right, up_left, up_right, down_left, down_right
                     open_set.append(neighbor.value)
+                    neighbor.set_color(red)
+                    father[neighbor.value] = current
+                    g.draw(sc)
+                if neighbor.color == purple:
+                    open_set.append(neighbor.value)
+                    father[neighbor.value] = current
 
-        closed_set.append(father[indx])
+        closed_set.append(current)
 
-        if g.grid_cells[father[indx]].color not in [orange,purple]:
-            g.grid_cells[father[indx]].set_color(blue)
+        if g.grid_cells[current].color not in [orange, purple]:
+            g.grid_cells[current].set_color(blue)
             g.draw(sc)
 
     raise NotImplementedError('Not implemented')
 
-def UCS(g:Graph, sc:pygame.Surface):
+
+def UCS(g: Graph, sc: pygame.Surface):
     print('Implement UCS algorithm')
 
     open_set = {}
     open_set[g.start.value] = 0
-    closed_set:list[int] = []
+    closed_set: list[int] = []
     father = [-1]*g.get_len()
     cost = [100_000]*g.get_len()
     cost[g.start.value] = 0
 
-    #TODO: Implement UCS algorithm using open_set, closed_set, and father
     raise NotImplementedError('Not implemented')
--- Source/Space.py
+++ Source/Space.py
@@ -2,8 +2,9 @@ import pygame
 from Constants import *
 import math
 
+
 class Node:
-    def __init__(self, x, y, value, radius = 10) -> None:
+    def __init__(self, x, y, value, radius=10) -> None:
         '''
         x,y: tọa độ của node
         value: giá trị của node, có thể dùng làm giá trị định danh
@@ -13,7 +14,7 @@ class Node:
         self.radius = radius
         self.color = green
 
-    def draw(self, sc:pygame.Surface) -> None:
+    def draw(self, sc: pygame.Surface) -> None:
         '''
         hàm vẽ node lên 1 bề mặt `sc`
         '''
@@ -30,26 +31,29 @@ class Node:
         '''
         self.color = color
 
+
 class Graph:
-    def __init__(self, start_pos:int, goal_pos:int) -> None:
+    def __init__(self, start_pos: int, goal_pos: int) -> None:
         '''
         khởi tạo đồ thị
         start_pos: vị trí bắt đầu
         goal_pos: vị trí đích
         các vị trí này chính là giá trị `value` của node
         '''
-        self.grid_cells:list[Node] = []
+        self.grid_cells: list[Node] = []
         for i in range(1, rows-1):
             for j in range(1, cols-1):
-                self.grid_cells.append(Node(j*TILE+TILE/2, i*TILE+TILE/2, (i-1)*(cols-2)+(j-1)))
+                self.grid_cells.append(
+                    Node(j*TILE+TILE/2, i*TILE+TILE/2, (i-1)*(cols-2)+(j-1)))
 
-        self.start:Node = self.grid_cells[start_pos]
+        self.start: Node = self.grid_cells[start_pos]
         self.start.set_color(orange)
-        self.goal:Node = self.grid_cells[goal_pos]
+        self.goal: Node = self.grid_cells[goal_pos]
         self.goal.set_color(purple)
 
         for i in self.grid_cells:
-            i.heuristic=math.sqrt(abs(i.x-self.goal.x)**2+abs(i.y-self.goal.y)**2)
+            i.heuristic = math.sqrt(
+                abs(i.x-self.goal.x)**2+abs(i.y-self.goal.y)**2)
 
     def get_len(self) -> int:
         '''
@@ -57,7 +61,7 @@ class Graph:
         '''
         return len(self.grid_cells)
 
-    def is_goal(self, node:Node) -> bool:
+    def is_goal(self, node: Node) -> bool:
         '''
         kiểm tra node `node` có phải là đích hay không
         '''
@@ -65,7 +69,7 @@ class Graph:
             return True
         return False
 
-    def draw(self, sc:pygame.Surface):
+    def draw(self, sc: pygame.Surface):
         '''
         vẽ đồ thị lên bề mặt `sc`
         '''
@@ -73,12 +77,12 @@ class Graph:
             node.draw(sc)
         pygame.display.flip()
 
-    def get_neighbors(self, node:Node):
+    def get_neighbors(self, node: Node):
         '''
         trả về các node kề của node `node` theo 8 hướng
         '''
         r = node.value//(cols-2)
-        c = node.value%(cols-2)
+        c = node.value % (cols-2)
 
         up = (r-1, c) if r-1 >= 0 else None
         down = (r+1, c) if r+1 < (rows-2) else None
@@ -90,7 +94,8 @@ class Graph:
         down_left = (r+1, c-1) if r+1 < (rows-2) and c-1 >= 0 else None
         down_right = (r+1, c+1) if r+1 < (rows-2) and c+1 < (cols-2) else None
 
-        directions = [up, down, left, right, up_left, up_right, down_left, down_right]
+        directions = [up, down, left, right,
+                      up_left, up_right, down_left, down_right]
         neighbors = []
         for dir in directions:
             if dir is not None:
